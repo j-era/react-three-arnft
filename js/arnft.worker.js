cdae17d5f7c54ecb018cdb618386363ff46ed6fa
Jens Eraßmy <jens.erassmy@artcom.de>
2022-05-02 11:42:19 +0200

    Adapt worker to use simd code

--- js/arnft.worker.js
+++ js/arnft.worker.js
@@ -1,5 +1,36 @@
 /* eslint-disable no-undef */
-importScripts("./ARToolkitNFT.js")
+
+const browser = (function () {
+  const test = function (regexp) {
+    return regexp.test(navigator.userAgent)
+  }
+  switch (true) {
+    case test(/edg/i):
+      return "Microsoft Edge"
+    case test(/trident/i):
+      return "Microsoft Internet Explorer"
+    case test(/firefox|fxios/i):
+      return "Mozilla Firefox"
+    case test(/opr\//i):
+      return "Opera"
+    case test(/ucbrowser/i):
+      return "UC Browser"
+    case test(/samsungbrowser/i):
+      return "Samsung Browser"
+    case test(/chrome|chromium|crios/i):
+      return "Google Chrome"
+    case test(/safari/i):
+      return "Apple Safari"
+    default:
+      return "Other"
+  }
+})()
+
+if (browser === "Apple Safari") {
+  importScripts("./ARToolkitNFT.js")
+} else {
+  importScripts("./ARToolkitNFT_simd.js")
+}
 
 let arController = null
 let currentMarkerResult = null
